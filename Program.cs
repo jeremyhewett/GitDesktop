892a99e8e8c9555bf3836ab1fc96825dc860ff84
Jeremy Hewett <jeremyhewett@gmail.com>
2018-01-26 11:39:09 -0800

    Initial commit, empty project.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GitDesktop
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
